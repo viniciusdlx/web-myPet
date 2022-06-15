c9e158258c2c756efce0acb695156d5425097077
Artur Salvador Tiscoski <artur.393414@alunosatc.edu.br>
2022-06-14 21:30:32 -0300

    FEAT[WIP]: Começa tela de listagem de pets

--- src/pages/MyPets.jsx
+++ src/pages/MyPets.jsx
@@ -3,10 +3,151 @@ import Navbar from '../components/Navbar'
 // import { Container } from './styles';
 
 function MyPets() {
+  const petsLists = [
+    {
+      name: 'Primeiro pet',
+      description: 'Esse pet é mto legal'
+    },
+    {
+      name: 'Segundo pet',
+      description: 'Esse pet é mto legal'
+    },
+    {
+      name: 'Terceiro pet',
+      description: 'Esse pet é mto legal'
+    },
+    {
+      name: 'Quarto pet',
+      description: 'Esse pet é mto legal'
+    },
+    {
+      name: 'Quinto pet',
+      description: 'Esse pet é mto legal'
+    },
+    {
+      name: 'Sexto pet',
+      description: 'Esse pet é mto legal'
+    },
+  ]
+
   return (
     <>
       <header>
         <Navbar />
+        <div className="flex items-center py-10">
+        <div className="container mx-auto text-white">
+          <div className="w-1/6 sm:w-3/4 lg:w-10/12 3xl:w-[25%] mx-auto">
+            <h1 className="text-[30px] lg:text-5xl text-mpPurple2 font-bold">
+              Meus pets
+            </h1>
+              <div>
+                <div class="accordion py-10" id="accordionExample">
+                  <div class="accordion-item bg-white border border-gray-200">
+                    <h2 class="accordion-header mb-0" id="headingOne">
+                      <button class="
+                        accordion-button
+                        relative
+                        flex
+                        items-center
+                        w-full
+                        py-4
+                        px-5
+                        text-base text-gray-800 text-left
+                        bg-white
+                        border-0
+                        rounded-none
+                        transition
+                        focus:outline-none
+                      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                        aria-controls="collapseOne">
+                        Accordion Item #1
+                      </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample">
+                      <div class="accordion-body py-4 px-5">
+                        <strong>This is the first item's accordion body.</strong> It is shown by default,
+                        until the collapse plugin adds the appropriate classes that we use to style each
+                        element. These classes control the overall appearance, as well as the showing and
+                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                        our default variables. It's also worth noting that just about any HTML can go within
+                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item bg-white border border-gray-200">
+                    <h2 class="accordion-header mb-0" id="headingTwo">
+                      <button class="
+                        accordion-button
+                        collapsed
+                        relative
+                        flex
+                        items-center
+                        w-full
+                        py-4
+                        px-5
+                        text-base text-gray-800 text-left
+                        bg-white
+                        border-0
+                        rounded-none
+                        transition
+                        focus:outline-none
+                      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                        aria-controls="collapseTwo">
+                        Accordion Item #2
+                      </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                      data-bs-parent="#accordionExample">
+                      <div class="accordion-body py-4 px-5">
+                        <strong>This is the second item's accordion body.</strong> It is hidden by default,
+                        until the collapse plugin adds the appropriate classes that we use to style each
+                        element. These classes control the overall appearance, as well as the showing and
+                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                        our default variables. It's also worth noting that just about any HTML can go within
+                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item bg-white border border-gray-200">
+                    <h2 class="accordion-header mb-0" id="headingThree">
+                      <button class="
+                        accordion-button
+                        collapsed
+                        relative
+                        flex
+                        items-center
+                        w-full
+                        py-4
+                        px-5
+                        text-base text-gray-800 text-left
+                        bg-white
+                        border-0
+                        rounded-none
+                        transition
+                        focus:outline-none
+                      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
+                        aria-controls="collapseThree">
+                        Accordion Item #3
+                      </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                      data-bs-parent="#accordionExample">
+                      <div class="accordion-body py-4 px-5">
+                        <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                        until the collapse plugin adds the appropriate classes that we use to style each
+                        element. These classes control the overall appearance, as well as the showing and
+                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                        our default variables. It's also worth noting that just about any HTML can go within
+                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   )
