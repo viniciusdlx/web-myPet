0392196e47649066b725d2ec0331db18b4fb7759
Vinicius de Lima Xavier <53457191+ViniciusDLx@users.noreply.github.com>
2022-06-20 05:41:18 -0300

    FEAT: Login com Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'tw-elements'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    // domain="my-pet.us.auth0.com"
+    domain="dev-s5h3lzwa.us.auth0.com"
+    // clientId="qECVeyW6eCPlPysAHRFPHNaohzU3EQJL"
+    clientId="XWFLTuuXYdMHNtZGInSsgBMF7i0TztYZ"
+    redirectUri={window.location.origin}
+    audience="https://dev-s5h3lzwa.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 )
--- src/pages/CadastroUser.jsx
+++ src/pages/CadastroUser.jsx
@@ -2,26 +2,80 @@ import React from 'react'
 import Background from '../components/Background'
 import Navbar from '../components/Navbar'
 
-function sendFormUser(e) {
-  e.preventDefault()
-  const formUser = document.getElementById('formUser')
-  const username = document.getElementById('username').value
-  const password = document.getElementById('password').value
-  const email = document.getElementById('email').value
-  const birthdate = document.getElementById('birthdate').value
-  const gender = document.getElementById('gender').value
-  const data = {
-    username: username,
-    password: password,
-    email: email,
-    birthdate: birthdate,
-    gender: gender
-  }
-  console.log(data)
-  formUser.reset()
+// function sendFormUser(e) {
+//   e.preventDefault()
+//   const formUser = document.getElementById('formUser')
+//   const username = document.getElementById('username').value
+//   const password = document.getElementById('password').value
+//   const email = document.getElementById('email').value
+//   const birthdate = document.getElementById('birthdate').value
+//   const gender = document.getElementById('gender').value
+//   const data = {
+//     username: username,
+//     password: password,
+//     email: email,
+//     birthdate: birthdate,
+//     gender: gender
+//   }
+//   console.log(data)
+//   formUser.reset()
+// }
+
+function submitFormTutor() {
+  const formTutor = document.getElementById('formTutor')
+  formTutor.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const formData = new FormData(this)
+    const searchParams = new URLSearchParams()
+
+    for (const pair of formData) {
+      searchParams.append(pair[0], pair[1])
+    }
+
+    formTutor.reset()
+  })
 }
 
 function CadastroUser() {
+  //   e.preventDefault()
+
+  //   const formTutor = new FormData(this)
+  //   const searchParams = new URLSearchParams()
+
+  //   for (const pair of formTutor) {
+  //     console.log(pair)
+  //   }
+
+  //   options = {
+  //     method: 'POST',
+  //     body: formTutor
+  //   }
+
+  //   fetch('https://', {
+  //     method: 'POST',
+  //     body: formData
+  //   })
+  //     .then(response => {
+  //       return response.text()
+  //     })
+  //     .then(data => {
+  //       alert('Produto Cadastrado com Sucesso')
+  //       console.log(data)
+  //     })
+
+  //   formCadastrarProduto.reset()
+  // })
+
+  document.getElementById('phone').addEventListener('input', function (e) {
+    var x = e.target.value
+      .replace(/\D/g, '')
+      .match(/(\d{0,3})(\d{0,3})(\d{0,4})/)
+    e.target.value = !x[2]
+      ? x[1]
+      : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '')
+  })
+
   const bgGradient =
     'bg-gradient-to-r from-mpGradientInit via-mpGradientMiddle to-mpGradientEnd'
 
@@ -38,45 +92,53 @@ function CadastroUser() {
             </h1>
             <div className="h-full border-2 border-white bg-white rounded-2xl shadow-xl mt-6 lg:mt-10">
               <form
-                action=""
-                id="formUser"
-                method="POST"
+                id="formTutor"
                 autoComplete="off"
                 className="flex flex-col justify-center items-center h-full p-4 gap-y-4 text-black"
               >
                 <input
                   type="text"
                   id="username"
+                  name="username"
                   autoComplete="name"
-                  className="ease-in-out duration-300 w-full py-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
+                  className="ease-in-out duration-300 w-full p-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
                   placeholder="Digite seu nome"
+                  required
                 />
                 <input
-                  type="password"
-                  id="password"
-                  className="ease-in-out duration-300 w-full py-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
-                  placeholder="Digite sua senha"
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  autoComplete="phone"
+                  className="ease-in-out duration-300 w-full p-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
+                  placeholder="Digite seu telefone"
+                  required
                 />
                 <input
                   type="email"
                   id="email"
+                  name="email"
                   autoComplete="email"
-                  className="ease-in-out duration-300 w-full py-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
+                  className="ease-in-out duration-300 w-full p-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic placeholder:text-black"
                   placeholder="Digite seu email"
+                  required
                 />
                 <input
                   type="date"
                   id="birthdate"
-                  className="ease-in-out duration-300 w-full py-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic"
+                  name="birthdate"
+                  autoComplete="birthdate"
+                  className="ease-in-out duration-300 w-full p-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic placeholder:italic"
+                  required
                 />
                 <select
                   name="gender"
                   id="gender"
-                  className="ease-in-out duration-300 w-full py-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic"
+                  autoComplete="gender"
+                  className="ease-in-out duration-300 w-full p-4 rounded-xl bg-mpGrey bg-opacity-10 border-1 border-slate-300 text-sm md:text-base lg:text-lg focus:border-none focus:ring-2 focus:border-mpPurple1 focus:ring-mpPurple1 italic"
+                  required
                 >
-                  <option value="nao selecionado" selected>
-                    Selecione o gênero
-                  </option>
+                  <option value="">Selecione o gênero</option>
                   <option value="masculino">Masculino</option>
                   <option value="feminino">Feminino</option>
                   <option value="outro">Outro</option>
@@ -84,7 +146,7 @@ function CadastroUser() {
                 <button
                   type="submit"
                   id="btnSendFormUser"
-                  onClick={sendFormUser}
+                  onClick={submitFormTutor}
                   className={`${bgGradient} text-2xl text-white px-10 py-3 rounded-full font-semibold tracking-wider`}
                 >
                   Cadastrar
